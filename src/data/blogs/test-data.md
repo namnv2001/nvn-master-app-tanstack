0ee4212caf8853897de4ddce258231b0b3208ccb
namnv2001 <namnv.forwork@gmail.com>
2025-12-05 22:57:29 +0700

    #patch: add markdown components

--- src/data/blogs/test-data.md
+++ src/data/blogs/test-data.md
@@ -5,15 +5,11 @@ lastModified: '2024-08-16'
 tags: ['next-js', 'tailwind', 'guide']
 draft: false
 summary: 'Looking for a performant, out of the box template, with all the best in web technology to support your blogging needs? Checkout the Tailwind Nextjs Starter Blog template.'
-images:
-  [
-    '/static/images/tanstack-circle-logo.png',
-    '/static/images/canada/toronto.jpg',
-  ]
+images: ['/static/images/DSCF1064.JPG', '/static/images/canada/toronto.jpg']
 authors: ['default', 'sparrowhawk']
 ---
 
-![tailwind-nextjs-banner](/static/images/tanstack-circle-logo.png)
+![tailwind-nextjs-banner](/static/images/DSCF1064.JPG)
 
 # Tailwind Nextjs Starter Blog
 
@@ -24,7 +20,7 @@ authors: ['default', 'sparrowhawk']
 
 This is a [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/) blogging starter template. Probably the most feature-rich Next.js markdown blogging template out there. Comes out of the box configured with the latest technologies to make technical writing a breeze. Easily configurable and customizable. Perfect as a replacement to existing Jekyll and Hugo individual blogs.
 
-Check out the documentation below to get started.
+~~Check out the documentation below to get started.~~
 
 Facing issues? Check the [FAQ page](https://github.com/timlrx/tailwind-nextjs-starter-blog/wiki) and do a search on past issues. Feel free to open a new issue if none has been posted previously.
 
@@ -86,8 +82,7 @@ I wanted it to be nearly as feature-rich as popular blogging templates like [bea
 
 1. JS (official support) - `npx degit https://github.com/timlrx/tailwind-nextjs-starter-blog.git` or TS (community support) - `npx degit timlrx/tailwind-nextjs-starter-blog#typescript`
 2. Personalize `siteMetadata.js` (site related information)
-3. Modify the content security policy in `next.config.js` if you want to use
-   any analytics provider or a commenting solution other than giscus.
+3. Modify the content security policy in `next.config.js` if you want to use any analytics provider or a commenting solution other than giscus.
 4. Personalize `authors/default.md` (main author)
 5. Modify `projectsData.ts`
 6. Modify `headerNavLinks.ts` to customize navigation links
--- src/routes/blog/$blogId.tsx
+++ src/routes/blog/$blogId.tsx
@@ -1,5 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
 
+import Markdown from 'react-markdown'
+import remarkBreaks from 'remark-breaks'
+import remarkGfm from 'remark-gfm'
+
+import { Image } from '@/components/Image'
 import { getBlogBySlug } from '@/data/blog'
 
 export const Route = createFileRoute('/blog/$blogId')({
@@ -9,6 +14,88 @@ export const Route = createFileRoute('/blog/$blogId')({
 
 function RouteComponent() {
   const blog = Route.useLoaderData()
-  console.log('blog', blog)
-  return <div>Hello "/blog/$blogId/$blogId"!</div>
+
+  return (
+    <Markdown
+      children={blog.content}
+      remarkPlugins={[remarkGfm, remarkBreaks]}
+      components={{
+        img: ({ node, ...props }) => <Image {...props} />,
+        a: ({ node, ...props }) => <Link to={props.href} {...props} />,
+        h1: ({ node, ...props }) => (
+          <h1 className="text-4xl font-bold mt-8 mb-4" {...props} />
+        ),
+        h2: ({ node, ...props }) => (
+          <h2 className="text-3xl font-bold mt-6 mb-3" {...props} />
+        ),
+        h3: ({ node, ...props }) => (
+          <h3 className="text-2xl font-bold mt-5 mb-2" {...props} />
+        ),
+        h4: ({ node, ...props }) => (
+          <h4 className="text-xl font-bold mt-4 mb-2" {...props} />
+        ),
+        h5: ({ node, ...props }) => (
+          <h5 className="text-lg font-bold mt-3 mb-2" {...props} />
+        ),
+        h6: ({ node, ...props }) => (
+          <h6 className="text-base font-bold mt-3 mb-2" {...props} />
+        ),
+        p: ({ node, ...props }) => <p className="mb-4 leading-7" {...props} />,
+        ul: ({ node, ...props }) => (
+          <ul className="list-disc list-inside mb-4 space-y-2" {...props} />
+        ),
+        ol: ({ node, ...props }) => (
+          <ol className="list-decimal list-inside mb-4 space-y-2" {...props} />
+        ),
+        li: ({ node, ...props }) => <li className="ml-4" {...props} />,
+        blockquote: ({ node, ...props }) => (
+          <blockquote
+            className="border-l-4 border-gray-300 pl-4 italic my-4"
+            {...props}
+          />
+        ),
+        code: ({ node, ...props }) => (
+          <code
+            className="inline-block bg-gray-100 dark:bg-gray-800 px-2 m-0 -mb-2 rounded"
+            {...props}
+          />
+        ),
+        pre: ({ node, ...props }) => (
+          <pre
+            className="bg-gray-100 dark:bg-gray-800 p-4 rounded my-4 overflow-x-auto"
+            {...props}
+          />
+        ),
+        hr: ({ node, ...props }) => (
+          <hr className="my-6 border-gray-300" {...props} />
+        ),
+        table: ({ node, ...props }) => (
+          <table
+            className="w-full border-collapse border border-gray-300 my-4"
+            {...props}
+          />
+        ),
+        thead: ({ node, ...props }) => (
+          <thead className="bg-gray-100 dark:bg-gray-800" {...props} />
+        ),
+        tbody: ({ node, ...props }) => <tbody {...props} />,
+        tr: ({ node, ...props }) => (
+          <tr className="border-b border-gray-300" {...props} />
+        ),
+        th: ({ node, ...props }) => (
+          <th
+            className="border border-gray-300 px-4 py-2 text-left font-bold"
+            {...props}
+          />
+        ),
+        td: ({ node, ...props }) => (
+          <td className="border border-gray-300 px-4 py-2" {...props} />
+        ),
+        strong: ({ node, ...props }) => (
+          <strong className="font-bold" {...props} />
+        ),
+        em: ({ node, ...props }) => <em className="italic" {...props} />,
+      }}
+    />
+  )
 }
