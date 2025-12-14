05213007b2c38f14f1368aa9de63ee83f2882676
namnv2001 <namnv.forwork@gmail.com>
2025-12-14 21:11:24 +0700

    #patch: update nav mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import { Link } from '@tanstack/react-router'
-
+import { Link, useLocation } from '@tanstack/react-router'
+import { List, ListXIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -7,7 +8,17 @@ import {
   NavigationMenuList,
 } from './ui/navigation-menu'
 
+import { cn } from '@/lib/utils'
+
+const MOBILE_WIDTH = 950
+
 export default function Header() {
+  const { pathname } = useLocation()
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(window.innerWidth < MOBILE_WIDTH)
+  const [activeItem, setActiveItem] = useState('')
+
   const navItems = [
     {
       label: 'Home',
@@ -23,13 +34,28 @@ export default function Header() {
     },
   ]
 
+  useEffect(() => {
+    setActiveItem(pathname)
+  }, [pathname])
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < MOBILE_WIDTH)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   const renderNavItems = () => {
     return navItems.map((item) => (
       <NavigationMenuItem key={item.label}>
         <NavigationMenuLink asChild>
           <Link
             to={item.to}
-            className="text-neutral-200 hover:text-foreground transition-colors font-bold"
+            className={cn(
+              'text-neutral-200 transition-colors',
+              activeItem === item.to && 'font-bold',
+            )}
           >
             {item.label}
           </Link>
@@ -39,11 +65,39 @@ export default function Header() {
   }
 
   return (
-    <NavigationMenu className="sticky top-0 z-50 w-full bg-nav-background">
-      <div className="container max-w-5xl mx-auto px-4 py-2 md:py-4 flex items-center justify-end">
-        <NavigationMenuList className="flex justify-between gap-4">
+    <NavigationMenu
+      className={cn(
+        'sticky top-0 z-50 w-full bg-nav-background',
+        isOpen && 'bg-nav-background-open',
+      )}
+    >
+      <div className="container max-w-5xl mx-auto px-4 py-4 flex items-center justify-end">
+        <NavigationMenuList
+          className={cn('flex justify-between gap-4', isMobile && 'hidden')}
+        >
           {renderNavItems()}
         </NavigationMenuList>
+
+        {/* Mobile menu trigger */}
+        <div
+          className={cn(
+            'text-neutral-200 cursor-pointer',
+            !isMobile && 'hidden',
+            isOpen && 'h-screen',
+          )}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <ListXIcon className="size-8" />
+          ) : (
+            <List className="size-8" />
+          )}
+
+          {/* Mobile menu content */}
+          {isOpen && (
+            <div className="bg-amber-400 list-none">{renderNavItems()}</div>
+          )}
+        </div>
       </div>
     </NavigationMenu>
   )
